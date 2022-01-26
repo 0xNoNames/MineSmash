75a8d2c41c3c222ff214bf8bfd8473ae5e2e95ad
Arthur Pêtre <arthur30700@gmail.com>
2022-01-26 01:07:20 +0100

    Ajout du spritede l'arc autour du joueur

---
+++ Assets/Bow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    [SerializeField] private Player player;
+    [SerializeField] private Camera cam;
+    [SerializeField] private SpriteRenderer sprite;
+
+    private Rigidbody2D playerRB;
+    private Collider2D playerColl;
+    private Vector2 mousePos;
+
+    private void Start()
+    {
+        playerRB = player.GetComponent<Rigidbody2D>();
+        playerColl = player.GetComponent<Collider2D>();
+    }
+
+    void Update() => mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    private void FixedUpdate()
+    {
+        Vector2 lookDir = mousePos - playerRB.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x);
+
+        float distance = 1f;
+
+        sprite.transform.position = new Vector2(playerRB.position.x + Mathf.Cos(angle) * distance, playerRB.position.y + Mathf.Sin(angle) * distance);
+
+        sprite.transform.eulerAngles = new Vector3(0, 0, angle * Mathf.Rad2Deg - 135f);
+    }
+}
