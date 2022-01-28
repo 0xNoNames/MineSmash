cca42819d0df6e2ff1345e91c205135888ad6702
Arthur Pêtre <arthur30700@gmail.com>
2022-01-28 17:18:22 +0100

    Améliorations au niveau de la visée
    - Modification de la touche de tir de la manette
    - Ajout deadzone sur le joystick de visé
    - La position de l'arc ne se réinitialise plus lorsque le joueur lâche le joystick

--- Assets/Scripts/BowAim.cs
+++ Assets/Scripts/BowAim.cs
@@ -9,39 +9,43 @@ public class BowAim : MonoBehaviour
     [SerializeField] private Rigidbody2D playerRB;
 
     private Camera cam;
-    private Vector2 aimPos;
+    private float distance = 0.75f;
+    private Vector2 inputVector;
     private bool isUsingMouse;
 
     private void Awake()
     {
         cam = FindObjectOfType<Camera>();
+
         if (GetComponentInParent<PlayerInput>().currentControlScheme == "Keyboard&Mouse")
             isUsingMouse = true;
         else
             isUsingMouse = false;
+
+        sprite.transform.position = new Vector2(playerRB.position.x + distance, playerRB.position.y);
+        sprite.transform.eulerAngles = new Vector3(0, 0, -90f);
     }
 
     private void FixedUpdate()
     {
-        Vector2 lookDir = aimPos;
+        Vector2 fixedInput = inputVector;
 
         if (isUsingMouse)
-            lookDir -= playerRB.position;
+            fixedInput = (Vector2)cam.ScreenToWorldPoint(inputVector) - playerRB.position;
 
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x);
-
-        float distance = 0.75f;
+        float angle = Mathf.Atan2(fixedInput.y, fixedInput.x);
 
         sprite.transform.position = new Vector2(playerRB.position.x + Mathf.Cos(angle) * distance, playerRB.position.y + Mathf.Sin(angle) * distance);
-
         sprite.transform.eulerAngles = new Vector3(0, 0, angle * Mathf.Rad2Deg - 90f);
     }
 
-    public void Aim(InputAction.CallbackContext inputAim)
+    public void Aim(InputAction.CallbackContext directionInput)
     {
-        aimPos = inputAim.ReadValue<Vector2>();
+        Vector2 rawInput = directionInput.ReadValue<Vector2>();
 
-        if (isUsingMouse)
-            aimPos = cam.ScreenToWorldPoint(aimPos);
+        if (rawInput.magnitude <= 0.1f)
+            return;
+
+        inputVector = rawInput;
     }
 }
